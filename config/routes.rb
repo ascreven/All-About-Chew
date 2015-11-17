2fcb666f947d023062a6f44711d92e2d7de24d9b
Alexandra <akscreven@gmail.com>
2015-11-17 14:14:44 -0500

    sign in finally working

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :profiles
   root 'welcome#index'
-  get '/sign_in', to: 'users#sign_in'
-  post '/sign_in', to: 'users#sign_in!'
-  get '/sign_up', to: 'users#sign_up'
-  post '/sign_up', to: 'users#sign_up!'
-  get '/sign_out', to: 'users#sign_out'
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
