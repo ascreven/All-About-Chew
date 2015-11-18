06a31c695cc6f8d65bf7484ed107b4c196dd19cd
Alexandra <akscreven@gmail.com>
2015-11-18 04:18:20 -0500

    rake db:schema is working

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  def index
+    if current_user
+      @profiles = current_user.profiles
+    else
+      @profiles = Profiles.all
+    end
+  end
+end
+end
---
+++ db/migrate/20151118084904_remove_line_items_from_profiles.rb
@@ -0,0 +1,5 @@
+class RemoveLineItemsFromProfiles < ActiveRecord::Migration
+  def change
+#         remove_column :line_items
+  end
+end
