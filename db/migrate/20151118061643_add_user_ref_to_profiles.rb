dfb938965cadd9f6e3291ab7237470f63214f18a
Alexandra <akscreven@gmail.com>
2015-11-18 12:51:22 -0500

    schema works

--- db/migrate/20151118061643_add_user_ref_to_profiles.rb
+++ db/migrate/20151118061643_add_user_ref_to_profiles.rb
@@ -1,5 +1,5 @@
 class AddUserRefToProfiles < ActiveRecord::Migration
   def change
-    add_reference :profiles, :user, index: true, foreign_key: true
+    # add_reference :profiles, :user, index: true, foreign_key: true
   end
 end
