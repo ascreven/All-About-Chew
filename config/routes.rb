ff0ed10fee633bae723c3166f36dba5c61ecb6c0
Alexandra <akscreven@gmail.com>
2015-11-19 03:37:30 -0500

    home button offered when you're signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :profiles
   root 'welcome#index'
+  devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' }
+
+
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
