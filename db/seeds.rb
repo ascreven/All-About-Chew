2e9ff3aede92d4946dcb34ab95e985a330ab408d
Alexandra <akscreven@gmail.com>
2015-11-19 17:33:31 -0500

    multiple seeds all linking correctly to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,12 @@
 
 User.destroy_all
 Profile.destroy_all
-  User.create!([
-    {email: "testadmin@mvmanor.co.uk", password: "testadminuser", password_confirmation: "testadminuser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
-    {email: "testuser@mvmanor.co.uk", password: "testuseraccount", password_confirmation: "testuseraccount", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:01", last_sign_in_at: "2015-02-06 14:03:01", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
-    {email: "testcustomer@customer.co.uk", password: "testcustomeruser", password_confirmation: "testcustomeruser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:44", last_sign_in_at: "2015-02-06 14:03:44", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
-  ])
+      user1 = User.create!(email: "testadmin@mvmanor.co.uk", password: "testadminuser", password_confirmation: "testadminuser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
+      user2 = User.create!(email: "testuser@mvmanor.co.uk", password: "testuseraccount", password_confirmation: "testuseraccount", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:01", last_sign_in_at: "2015-02-06 14:03:01", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
+      user3 = User.create!(email: "testcustomer@customer.co.uk", password: "testcustomeruser", password_confirmation: "testcustomeruser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:44", last_sign_in_at: "2015-02-06 14:03:44", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
   Profile.create!([
-    {name: "Chloe", gender: "female", breed: "German Shepard", city: "Tucson", state: "Arizona", age: 15, pro_pic: "raybans.jpg", description: "Test 1", id: 4}
+    {name: "Chloe", gender: "female", breed: "German Shepard", city: "Tucson", state: "Arizona", age: 15, pro_pic: "raybans.jpg", description: "Test 1", user: user1},
+    {name: "Ella", gender: "female", breed: "German Shepard", city: "Tucson", state: "Arizona", age: 15, pro_pic: "raybans.jpg", description: "Test 1", user: user2},
+    {name: "Charlie", gender: "male", breed: "German Shepard", city: "Tucson", state: "Arizona", age: 15, pro_pic: "raybans.jpg", description: "Test 1", user: user3},
+    {name: "Angel", gender: "female", breed: "German Shepard", city: "Tucson", state: "Arizona", age: 15, pro_pic: "raybans.jpg", description: "Test 1", user: user2}
   ])
